1fc2c148caaa469a25b0141965cce06eb17873d8
Dallag0 <102759388+Dallag0@users.noreply.github.com>
2022-07-03 20:36:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-var user = prompt('Insira um login')
-var senha = prompt('agora insira uma senha')
-
 function login() {
   var user = prompt('Insira um login')
   var senha = prompt('agora insira uma senha')
 }
 
-function se() {
-  if (document.getElementById('login') == user && document.getElementById('password') == senha) {
-    alert('Login ou senha incorreto')
-  } else {
+if (onclick(login())) {
+  if (
+    document.getElementById('login') === login(user) &&
+    document.getElementById('password') === login(senha)
+  ) {
     alert('você entrou')
+  } else {
+    alert('Login ou senha incorreto')
+  }
+} else {
+  var usuario = prompt('Insira um login')
+  var password = prompt('Agora insira uma senha')
+
+  if (
+    document.getElementById('login') === usuario &&
+    document.getElementById('password') === password
+  ) {
+    alert('você entrou')
+  } else {
+    alert('Login ou senha incorreto')
   }
 }
